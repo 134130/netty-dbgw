aa97340fcb9a5b75cf176da4f23ebb99278e076d
JiHwan Oh <hwan_@kakao.com>
2025-07-15 19:42:58 +0900

    feat: add KDoc for peek function

--- utils/src/main/kotlin/com/github/l34130/netty/dbgw/utils/netty/ByteBufExtensions.kt
+++ utils/src/main/kotlin/com/github/l34130/netty/dbgw/utils/netty/ByteBufExtensions.kt
@@ -3,6 +3,12 @@ package com.github.l34130.netty.dbgw.utils.netty
 import io.netty.buffer.ByteBuf
 import java.io.EOFException
 
+/**
+ * Reads a slice of the ByteBuf without modifying the reader index.
+ * This allows you to peek at the data without consuming it.
+ * @param action The action to perform on the ByteBuf slice.
+ * @return The result of the action, or null if the ByteBuf is empty.
+ */
 fun <T> ByteBuf.peek(action: (ByteBuf) -> T): T? {
     if (readableBytes() == 0) {
         return null
