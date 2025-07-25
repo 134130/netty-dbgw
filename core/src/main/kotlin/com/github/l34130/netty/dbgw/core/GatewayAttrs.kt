9cef0a0a12562aa768480aa08c84bf07177b0962
JiHwan Oh <hwan_@kakao.com>
2025-07-25 21:38:47 +0900

    feat: Add business event executor group attribute to GatewayAttrs and set it in ProxyConnectionHandler

--- core/src/main/kotlin/com/github/l34130/netty/dbgw/core/GatewayAttrs.kt
+++ core/src/main/kotlin/com/github/l34130/netty/dbgw/core/GatewayAttrs.kt
@@ -4,12 +4,19 @@ import com.github.l34130.netty.dbgw.core.config.GatewayConfig
 import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
 import io.netty.util.AttributeKey
+import io.netty.util.concurrent.EventExecutorGroup
 
 object GatewayAttrs {
+    val BUSINESS_EVENT_EXECUTOR_GROUP_ATTR_KEY: AttributeKey<EventExecutorGroup> = AttributeKey.valueOf("business-executor-group")
     val DOWNSTREAM_ATTR_KEY: AttributeKey<Channel> = AttributeKey.valueOf("downstream")
     val UPSTREAM_ATTR_KEY: AttributeKey<Channel> = AttributeKey.valueOf("upstream")
     val GATEWAY_CONFIG_ATTR_KEY: AttributeKey<GatewayConfig> = AttributeKey.valueOf("config")
 }
+
+fun ChannelHandlerContext.businessEventExecutorGroup(): EventExecutorGroup =
+    checkNotNull(this.channel().attr(GatewayAttrs.BUSINESS_EVENT_EXECUTOR_GROUP_ATTR_KEY).get()) {
+        "Business event executor group is not set in the context"
+    }
 
 fun ChannelHandlerContext.downstream(): Channel =
     checkNotNull(this.channel().attr(GatewayAttrs.DOWNSTREAM_ATTR_KEY).get()) {
