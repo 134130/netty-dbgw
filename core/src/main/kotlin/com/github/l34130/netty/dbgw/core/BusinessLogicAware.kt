d90964ef0a536779ed03ab9fcdba13ae48527a7d
JiHwan Oh <hwan_@kakao.com>
2025-07-26 12:00:53 +0900

    refactor: improve documentation for BusinessLogicAware interface

--- core/src/main/kotlin/com/github/l34130/netty/dbgw/core/BusinessLogicAware.kt
+++ core/src/main/kotlin/com/github/l34130/netty/dbgw/core/BusinessLogicAware.kt
@@ -1,7 +1,7 @@
 package com.github.l34130.netty.dbgw.core
 
 /**
- * The [GatewayState] or [MessageInterceptor] that implements this interface contains business logic, so that it
+ * Marker interface for [GatewayState] or [MessageInterceptor] which contains business logic, so that it
  * runs in a separate pool of business threads to avoid blocking Netty's EventLoop threads.
  */
 interface BusinessLogicAware
