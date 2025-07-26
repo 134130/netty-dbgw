a4af5013bfba0e63729a3381f96056c33a382aed
JiHwan Oh <hwan_@kakao.com>
2025-07-26 23:11:30 +0900

    refactor: change readValues function visibility to internal in JacksonExtensions

--- policy/src/main/kotlin/com/github/l34130/netty/dbgw/policy/api/JacksonExtensions.kt
+++ policy/src/main/kotlin/com/github/l34130/netty/dbgw/policy/api/JacksonExtensions.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
 
-inline fun <reified T> ObjectMapper.readValues(content: String): List<T> {
+internal inline fun <reified T> ObjectMapper.readValues(content: String): List<T> {
     val parser: JsonParser = this.factory.createParser(content)
     return this.readValues(parser, jacksonTypeRef<T>()).readAll()
 }
