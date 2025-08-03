0c4dbe7eee884e6f5a129ab09584c0746b24ae9a
JiHwan Oh <hwan_@kakao.com>
2025-08-03 14:17:59 +0900

    refactor: update DatabaseResultSetMaskingPolicyDefinitionFactory to use DatabaseResultSetMaskingPolicyDefinition and adjust masking regex handling

--- policy-builtin/src/main/kotlin/com/github/l34130/netty/dbgw/policy/builtin/database/query/DatabaseResultSetMaskingPolicyDefinitionFactory.kt
+++ policy-builtin/src/main/kotlin/com/github/l34130/netty/dbgw/policy/builtin/database/query/DatabaseResultSetMaskingPolicyDefinitionFactory.kt
@@ -3,9 +3,9 @@ package com.github.l34130.netty.dbgw.policy.builtin.database.query
 import com.github.l34130.netty.dbgw.policy.api.config.AbstractResourceFactory
 
 class DatabaseResultSetMaskingPolicyDefinitionFactory :
-    AbstractResourceFactory<DatabaseResultSetMaskingPolicy>(DatabaseResultSetMaskingPolicy::class) {
-    override fun create(props: Map<String, Any>): DatabaseResultSetMaskingPolicy =
-        DatabaseResultSetMaskingPolicy(
-            maskingRegex = Regex(props["maskingRegex"] as String),
+    AbstractResourceFactory<DatabaseResultSetMaskingPolicyDefinition>(DatabaseResultSetMaskingPolicyDefinition::class) {
+    override fun create(props: Map<String, Any>): DatabaseResultSetMaskingPolicyDefinition =
+        DatabaseResultSetMaskingPolicyDefinition(
+            maskingRegex = props["maskingRegex"] as String,
         )
 }
